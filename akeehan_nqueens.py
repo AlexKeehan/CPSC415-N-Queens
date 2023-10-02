d2e6d6bb9edb22c75c02b55ffd6b03faae1aa7eb
Alex <alexk2678@yahoo.com>
2023-10-02 09:42:09 -0400

    Forgot to commit until now. Added function to check for conflicts and added function to check for successor states.

--- akeehan_nqueens.py
+++ akeehan_nqueens.py
@@ -1,37 +1,64 @@
 #!/usr/bin/env python3
 import sys
-import numpy as np
 import random
+
 size = int(sys.argv[1])
-
-board = [[0]* size for _ in range(size)]
-
+board = []
 
 for i in range(size):
-    
     rand = random.randint(0, size - 1)
-    board[rand][i] = "Q"
+    board.append(rand)
 print(board)
 
 
-# Check for conflicts
-conflicts = 0
-pairs = 0
-print(board[0])
-for x in range(size):
-    rqueens = 0
-    for i in range(size):
+# Get conflicts
+def get_conflicts(board):
+    # use num to iterate through board
+    num = len(board)
+    # Keep track of conflicts
+    conflicts = 0
+
     
-        if board[x][i] == "Q":
-            
-        if rqueens == 4:
-            conflicts = conflicts + 6
-        elif rqueens == 3:
-            conflicts = conflicts + 3
-        elif rqueens == 2:
-            conflicts = conflicts + 1
-    
-    
-    x = x + 1
+    for col in range(num):
+        for i in range(col + 1, num):
+            # Check for queens in same row        
+            if board[col] == board[i]:
+                conflicts += 1
+            # Check if queens are in same diagonal from top left down
+            elif board[col] - board[i] == col - i:
+                conflicts += 1
+            # Check if queens are in same diagonal from top right down
+            elif board[col] - board[i] == i - col:
+                conflicts += 1
+    return conflicts
+
+# Get possible successor states
+def get_successor_states(board):
+    # Use num to iterate through board
+    num = len(board)
+    # Keep track of successors
+    successors = []
+
+    for col in range(num):
+        curr_row = board[col]
+        # Use row as the "random" number to generate successor states
+        for row in range(num):
+            # Don't generate same state as one we are in
+            if row == curr_row:
+                continue
+            # Usin list to copy board over
+            temp_board = list(board)
+            # Changing just the one col using row as the "random" number
+            temp_board[col] = row
+                
+            # Add to successors
+            successors.append(temp_board)
+    return successors
+
+conflicts = get_conflicts(board)
 print("Conflicts", conflicts)
-#print(conflicts)
+successors = get_successor_states(board)
+print("Successors", successors)
+
+
+
